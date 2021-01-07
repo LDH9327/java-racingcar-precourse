7331bfb1eae4556b36a398b0d78b6b34e845f9ff
LDH9327 <dlarkfl2@gmail.com>
2021-01-08 00:02:33 +0900

    feat: 자동차 등록 기능 추가

---
+++ src/main/java/racingcar/view/screen/action/CarAddScreen.java
@@ -0,0 +1,28 @@
+package racingcar.view.screen.action;
+
+import java.util.List;
+import racingcar.car.CarRepository;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import racingcar.view.screen.Screen;
+import racingcar.view.screen.ScreenManager;
+
+public class CarAddScreen implements Screen {
+    public static final String ADD_CAR_GUIDE_MESSAGE = "등록할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String ADD_CAR_RESULT_MESSAGE = "경주 자동차가 등록되었습니다.";
+
+    @Override
+    public void visualize() {
+        OutputView.newLine();
+        OutputView.printlnGuide(ADD_CAR_GUIDE_MESSAGE);
+    }
+
+    @Override
+    public void logic(InputView inputView) {
+        List<String> carNameList = inputView.readCarNames();
+        CarRepository.addCarByNameList(carNameList);
+        OutputView.printlnResult(ADD_CAR_RESULT_MESSAGE);
+        OutputView.printlnCarListByName(CarRepository.cars());
+        ScreenManager.pop();
+    }
+}
