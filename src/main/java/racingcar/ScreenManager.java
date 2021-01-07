507fa0688991b4545b70d4090c29383c2cea213e
LDH9327 <dlarkfl2@gmail.com>
2021-01-07 13:57:36 +0900

    feat: Stack 구조 ScreenManager 클래스 추가

---
+++ src/main/java/racingcar/ScreenManager.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import java.util.Stack;
+import racingcar.view.screen.Screen;
+
+public class ScreenManager {
+
+    public static Stack<Screen> screenManager = new Stack<>();
+
+    public static boolean isEmpty() {
+        if (screenManager.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static Screen pop() {
+        return screenManager.pop();
+    }
+
+    public static Screen peek() {
+        return screenManager.peek();
+    }
+
+    public static void push(Screen screen) {
+        screenManager.push(screen);
+    }
+
+    public static void clear() {
+        screenManager.clear();
+    }
+}
