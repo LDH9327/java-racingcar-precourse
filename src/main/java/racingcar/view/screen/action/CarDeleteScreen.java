eaa128b29be5d979b9c5713b68a65b2345e4e76b
LDH9327 <dlarkfl2@gmail.com>
2021-01-08 00:02:47 +0900

    feat: 자동차 삭제 기능 추가

---
+++ src/main/java/racingcar/view/screen/action/CarDeleteScreen.java
@@ -0,0 +1,28 @@
+package racingcar.view.screen.action;
+
+import racingcar.car.CarRepository;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import racingcar.view.screen.Screen;
+import racingcar.view.screen.ScreenManager;
+
+public class CarDeleteScreen implements Screen {
+
+    public static final String DELETE_CAR_GUIDE_MESSAGE = "삭제할 자동차 이름을 입력하세요.";
+    public static final String DELETE_CAR_RESULT_MESSAGE = "경주 자동차가 삭제되었습니다.";
+
+    @Override
+    public void visualize() {
+        OutputView.newLine();
+        OutputView.printlnGuide(DELETE_CAR_GUIDE_MESSAGE);
+    }
+
+    @Override
+    public void logic(InputView inputView) {
+        String carName = inputView.readCarName();
+        CarRepository.deleteCarByName(carName);
+        OutputView.printlnResult(DELETE_CAR_RESULT_MESSAGE);
+        OutputView.printlnCarListByName(CarRepository.cars());
+        ScreenManager.pop();
+    }
+}
