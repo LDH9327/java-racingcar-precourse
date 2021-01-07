358fa124ae72ac9a0eab458a8ab4d6791bae4d1b
LDH9327 <dlarkfl2@gmail.com>
2021-01-08 00:01:11 +0900

    feat: 결과 출력, 저장된 자동차 이름을 출력하는 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,31 +1,53 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.car.Car;
+
 public class OutputView {
 
-  private static final String GUIDE_PREFIX = "##";
-  private static final String ERROR_PREFIX = "[ERROR]";
-  private static final String SPACE = " ";
+    private static final String GUIDE_PREFIX = "##";
+    private static final String ERROR_PREFIX = "[ERROR]";
+    private static final String RESULT_PREFIX = "[INFO]";
+    private static final String SPACE = " ";
+    private static final String COMMA = ",";
+    private static final String CAR_RESULT_MESSAGE = "현재 등록된 자동차: '%s'";
 
-  private OutputView() {
-  }
+    private OutputView() {
+    }
 
-  public static void newLine() {
-    System.out.println();
-  }
+    public static void newLine() {
+        System.out.println();
+    }
 
-  public static void println(String message) {
-    System.out.println(message);
-  }
+    public static void println(String message) {
+        System.out.println(message);
+    }
 
-  public static void printlnError(String errorMessage) {
-    println(String.join(SPACE, ERROR_PREFIX, errorMessage));
-  }
+    public static void printlnError(String errorMessage) {
+        println(String.join(SPACE, ERROR_PREFIX, errorMessage));
+    }
 
-  public static void printlnGuide(String guideMessage) {
-    println(String.join(SPACE, GUIDE_PREFIX, guideMessage));
-  }
+    public static void printlnResult(String resultMessage) {
+        println(String.join(SPACE, RESULT_PREFIX, resultMessage));
+    }
 
-  public static void printlnList(String command, String actionName) {
-    println(String.join(SPACE, command, actionName));
-  }
+    public static void printlnCarListByName(List<Car> carList) {
+        String carNameList = "";
+        for (Car car : carList) {
+            if (carNameList.isEmpty()) {
+                carNameList = car.getName();
+                continue;
+            }
+            carNameList = String.join(COMMA, carNameList, car.getName());
+        }
+        printlnResult(String.format(CAR_RESULT_MESSAGE, carNameList));
+    }
+
+    public static void printlnGuide(String guideMessage) {
+        println(String.join(SPACE, GUIDE_PREFIX, guideMessage));
+    }
+
+    public static void printlnList(String command, String actionName) {
+        println(String.join(SPACE, command, actionName));
+    }
 }
