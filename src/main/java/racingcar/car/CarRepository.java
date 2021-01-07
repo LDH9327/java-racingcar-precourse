5487208af82af17a4eb9cbea5023bb017e9dfa15
LDH9327 <dlarkfl2@gmail.com>
2021-01-07 23:58:55 +0900

    feat: Car객체를 저장하는 클래스 생성

---
+++ src/main/java/racingcar/car/CarRepository.java
@@ -0,0 +1,42 @@
+package racingcar.car;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import racingcar.exception.CannotFindCarInCarsException;
+import racingcar.exception.DuplicatedCarInCarsException;
+
+public class CarRepository {
+
+    private static final List<Car> cars = new ArrayList<>();
+
+    public static List<Car> cars() {
+        return Collections.unmodifiableList(cars);
+    }
+
+    private static void addCar(Car car) {
+        if (isCarInCars(car.getName())) {
+            throw new DuplicatedCarInCarsException(car.getName());
+        }
+        cars.add(car);
+    }
+
+    public static void addCarByNameList(List<String> carNameList) {
+        for (String carName : carNameList) {
+            addCar(new Car(carName));
+        }
+    }
+
+    public static void deleteCarByName(String carName) {
+        if (!isCarInCars(carName)) {
+            throw new CannotFindCarInCarsException(carName);
+        }
+        cars.removeIf(car -> Objects.equals(car.getName(), carName));
+    }
+
+    private static boolean isCarInCars(String carName) {
+        return cars.stream()
+                   .anyMatch(car -> Objects.equals(car.getName(), carName));
+    }
+}
