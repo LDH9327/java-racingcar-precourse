1fc41c1c2ad82ca6069574560c970a578bdbc0ea
LDH9327 <dlarkfl2@gmail.com>
2021-01-07 13:36:37 +0900

    feat: Application 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,22 @@
 package racingcar;
 
 import java.util.Scanner;
+import racingcar.view.InputView;
+import racingcar.view.screen.MainScreen;
 
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        InputView inputView = new InputView(scanner);
+        ScreenManager.push(new MainScreen());
+        run(inputView);
+    }
+
+    public static void run(InputView inputView) {
+        do {
+            ErrorHandler.goBackWhenException(inputView);
+        } while (!ScreenManager.isEmpty());
     }
 }
